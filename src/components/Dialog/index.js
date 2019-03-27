e969cd71cf8a2f01fcb1947bceb5194063bbb407
Colin Gagnon <gagnon.colin@gmail.com>
2019-03-27 18:37:36 -0400

    fix: Adds dialog component

---
+++ src/components/Dialog/index.js
@@ -0,0 +1,14 @@
+import { Dialog } from '@rmwc/dialog' 
+
+import './Dialog.scss'
+
+// Dialog included directly from: https://jamesmfriedman.github.io/rmwc/dialogs
+export {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  DialogButton
+} from '@rmwc/dialog'
+
+export default Dialog
