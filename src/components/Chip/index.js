db804434a598a7f07878c6a0b1612fcaeda7fd0f
Colin Gagnon <gagnon.colin@gmail.com>
2019-04-22 12:10:00 -0400

    fix: Adds chips

---
+++ src/components/Chip/index.js
@@ -0,0 +1,11 @@
+import { Chip } from '@rmwc/chip' 
+
+import './Chips.scss'
+
+// Chip included directly from: https://jamesmfriedman.github.io/rmwc/chips
+export {
+  Chip,
+  ChipSet
+} from '@rmwc/chip'
+
+export default Chip
